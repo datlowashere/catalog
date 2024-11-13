abfd677bd5bc684cedfd08703d54a470bf83f7b2
Đạt <datlowashere@gmail.com>
2024-11-13 11:30:31 +0700

    initial project

---
+++ lib/config/theme/app_text_styles.dart
@@ -0,0 +1,55 @@
+import 'package:catalog/config/theme/app_colors.dart';
+import 'package:flutter/material.dart';
+
+enum AppFontFamily { beVietNamPro }
+
+enum AppFontWeight { regular, medium, semiBold, bold }
+
+extension AppFontFamilyDefault on AppFontFamily {
+  String get name {
+    switch (this) {
+      case AppFontFamily.beVietNamPro:
+        return 'BeVietNamPro';
+    }
+  }
+}
+
+extension BeVietNamProFontWeight on AppFontWeight {
+  FontWeight get weight {
+    switch (this) {
+      case AppFontWeight.regular:
+        return FontWeight.w400;
+      case AppFontWeight.medium:
+        return FontWeight.w500;
+      case AppFontWeight.semiBold:
+        return FontWeight.w600;
+      case AppFontWeight.bold:
+        return FontWeight.w700;
+    }
+  }
+}
+
+class AppTextStyles {
+  const AppTextStyles._();
+
+  static BeVietNamProStyles beVietNamProStyles = BeVietNamProStyles();
+}
+
+class BeVietNamProStyles {
+  static TextStyle customBeVietNamProStyle(
+      double size,
+      Color color,
+      FontWeight fontWeight,
+      ) {
+    return TextStyle(
+      fontFamily: AppFontFamily.beVietNamPro.name,
+      color: color,
+      fontWeight: fontWeight,
+      fontSize: size,
+    );
+  }
+
+  final regular16Tiffany =
+  customBeVietNamProStyle(16, AppColors.tiffanyBlue, AppFontWeight.regular.weight);
+
+}
