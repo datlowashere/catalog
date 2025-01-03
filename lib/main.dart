2343629d69594bdf01c6843e6e19b3764ce99e57
Đạt <datlowashere@gmail.com>
2025-01-03 22:36:54 +0700

    test ui tabbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,72 +1,210 @@
-import 'package:catalog/config/theme/app_text_styles.dart';
 import 'package:flutter/material.dart';
+import 'package:scrollable_list_tab_scroller/scrollable_list_tab_scroller.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Scrollable List Tab Scroller',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Scrollable List Tab Scroller'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
+  MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  final data = {
+    "Category A": [
+      "Item 1 (A)",
+      "Item 2 (A)",
+      "Item 3 (A)",
+      "Item 4 (A)",
+      "Item 1 (A)",
+      "Item 2 (A)",
+      "Item 3 (A)",
+      "Item 4 (A)",
+      "Item 1 (A)",
+      "Item 2 (A)",
+      "Item 3 (A)",
+      "Item 4 (A)",
+      "Item 1 (A)",
+      "Item 2 (A)",
+      "Item 3 (A)",
+      "Item 4 (A)",
+      "Item 1 (A)",
+      "Item 2 (A)",
+      "Item 3 (A)",
+      "Item 4 (A)",
+    ],
+    "Category B": [
+      "Item 1 (B)",
+      "Item 2 (B)",
+      "Item 1 (B)",
+      "Item 2 (B)",
+      "Item 1 (B)",
+      "Item 2 (B)",
+      "Item 1 (B)",
+      "Item 2 (B)",
+      "Item 1 (B)",
+      "Item 2 (B)",
+      "Item 1 (B)",
+      "Item 2 (B)",
+      "Item 1 (B)",
+      "Item 2 (B)",
+      "Item 1 (B)",
+      "Item 2 (B)",
+    ],
+    "Category C": [
+      "Item 1 (C)",
+      "Item 2 (C)",
+      "Item 3 (C)",
+      "Item 4 (C)",
+      "Item 5 (C)",
+      "Item 1 (C)",
+      "Item 2 (C)",
+      "Item 3 (C)",
+      "Item 4 (C)",
+      "Item 5 (C)",
+      "Item 1 (C)",
+      "Item 2 (C)",
+      "Item 3 (C)",
+      "Item 4 (C)",
+      "Item 5 (C)",
+      "Item 1 (C)",
+      "Item 2 (C)",
+      "Item 3 (C)",
+      "Item 4 (C)",
+      "Item 5 (C)",
+    ],
+    "Category D": [
+      "Item 1 (D)",
+      "Item 2 (D)",
+      "Item 3 (D)",
+      "Item 4 (D)",
+      "Item 5 (D)",
+      "Item 1 (D)",
+      "Item 2 (D)",
+      "Item 3 (D)",
+      "Item 4 (D)",
+      "Item 5 (D)",
+      "Item 1 (D)",
+      "Item 2 (D)",
+      "Item 3 (D)",
+      "Item 4 (D)",
+      "Item 5 (D)",
+      "Item 1 (D)",
+      "Item 2 (D)",
+      "Item 3 (D)",
+      "Item 4 (D)",
+      "Item 5 (D)",
+    ],
+    "Category E": [
+      "Item 1 (E)",
+      "Item 2 (E)",
+      "Item 3 (E)",
+      "Item 4 (E)",
+      "Item 5 (E)",
+    ],
+    "Category F": [
+      "Item 1 (F)",
+      "Item 2 (F)",
+      "Item 3 (F)",
+      "Item 4 (F)",
+      "Item 5 (F)",
+    ],
+    "Category G": [
+      "Item 1 (G)",
+      "Item 2 (G)",
+      "Item 3 (G)",
+      "Item 4 (G)",
+      "Item 5 (G)",
+    ],
+    "Category H": [
+      "Item 1 (H)",
+      "Item 2 (H)",
+      "Item 3 (H)",
+      "Item 4 (H)",
+      "Item 5 (H)",
+    ],
+    "Category I": [
+      "Item 1 (I)",
+      "Item 2 (I)",
+      "Item 3 (I)",
+      "Item 4 (I)",
+      "Item 5 (I)",
+      "Item 6 (I)",
+      "Item 7 (I)",
+      "Item 8 (I)",
+      "Item 9 (I)",
+    ],
+  };
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+      body: ScrollableListTabScroller.defaultComponents(
+        headerContainerProps: const HeaderContainerProps(height: 50),
+        tabBarProps: TabBarProps(dividerColor: Colors.red.withOpacity(0.3)),
+        itemCount: data.length,
+        earlyChangePositionOffset: 30,
+        tabBuilder: (BuildContext context, int index, bool active) => Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Text(
+            data.keys.elementAt(index),
+            style: !active
+                ? null
+                : const TextStyle(
+                    fontWeight: FontWeight.bold, color: Colors.green),
+          ),
+        ),
+        itemBuilder: (BuildContext context, int index) => Column(
+          children: [
             Text(
-              'You have pushed the button this many times:',
-              style: AppTextStyles.beVietNamProStyles.regular16Tiffany,
+              data.keys.elementAt(index),
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            ...data.values
+                .elementAt(index)
+                .asMap()
+                .map(
+                  (index, value) => MapEntry(
+                    index,
+                    ListTile(
+                      leading: Container(
+                        height: 40,
+                        width: 40,
+                        decoration: const BoxDecoration(
+                            shape: BoxShape.circle, color: Colors.grey),
+                        alignment: Alignment.center,
+                        child: Text(index.toString()),
+                      ),
+                      title: Text(value),
+                    ),
+                  ),
+                )
+                .values
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
